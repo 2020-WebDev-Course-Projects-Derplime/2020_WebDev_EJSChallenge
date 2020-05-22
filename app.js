2eb4a0f48e7e29ae8a6cdb9dd9de6ab5230336de
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-22 04:15:52 -0400

    Challenge 2 done

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");  
+  res.render("home", { homeParagraph: homeStartingContent });  
 });
 
 
